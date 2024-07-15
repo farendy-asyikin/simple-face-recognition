c42b7d7574974d9df0c3bb502c9394bda14ba25b
farendy.asyikin <farendy.asyikin@gmail.com>
2024-07-15 19:27:35 +0700

    initial commit

---
+++ setings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'farendfal',
+        'PASSWORD': 's3crEt',
+        'HOST': 'localhost',
+        'PORT': '5433',
+    }
+}
